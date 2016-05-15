7eb38415f4a4ea69545554ffb057fccffb8e7928
M.Mykhalyuk <mim@qapint.com>
2016-05-15 14:30:02 +0300

    add ValidateInput component

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,31 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
- ReactDOM.render(
-	<h1>Hello, world!</h1>,
-	document.getElementById('example')
-);
+const ValidateInput = React.createClass({
+	getInitialState() {
+		return { valid: false };
+	},
+
+	validate(event) {
+		this.setState({ valid: event.target.value.includes('react') });
+	},
+
+	render() {
+		return (
+			<div>
+				<h2>Validate to 'react' value</h2>
+				<input onChange={this.validate} 
+					style={
+						{
+							background: this.state.valid ? 'green' : 'red',
+						 	height: 30,
+						 	width: 230,
+						 	fontSize: 24
+						 }
+					} />
+			</div>
+		);
+	}
+});
+
+ReactDOM.render(<ValidateInput />, document.getElementById('app'));
