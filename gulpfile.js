79f09673b1b32ca35f276d8fbec72a9f198a8b4a
M.Mykhalyuk <mim@qapint.com>
2016-05-15 14:05:16 +0300

    update(gulpfile.js): clean bundle

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 const gulp = require('gulp');
 const path = require('path');
+const del = require('del');
+const rename = require('gulp-rename');
 const babel = require('gulp-babel');
 const watch = require('gulp-watch');
 const babelEs6 = require('babel-preset-es2015');
 const babelReact = require('babel-preset-react');
 const browserify = require('gulp-browserify');
 
-gulp.task('watch-babel', () => {
+gulp.task('bundle', ['clean-bundle'], () => {
 	return gulp.src('src/js/**.js')
 		.pipe(watch('src/js/**.js', logChanges))
 		.pipe(babel({
@@ -15,8 +17,11 @@ gulp.task('watch-babel', () => {
 		.pipe(browserify({
 			debug : true
 		}))
+		.pipe(rename('bundle.js'))
 		.pipe(gulp.dest('dist/js/'));
 });
+
+gulp.task('clean-bundle', () => del('dist/js/*.*', {force: true}))
 
 function logChanges(file){
 	 console.log('File ' + path.relative(process.cwd(), file.history[0]) + ' was changed');
